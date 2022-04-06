d6e72767adb9ae45b67af5be2c5ee599d52bcfdf
sohalt <sohalt@sohalt.net>
2022-04-06 11:35:30 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  outputs = { ... }:
+    {
+      overlay = final: prev: {
+        writeBabashkaApplication =
+          { name
+          , text
+          , runtimeInputs ? [ ]
+          , checkPhase ? null
+          }:
+          let
+            script = final.writeText "script.clj" text;
+          in
+          final.writeShellApplication {
+            inherit name runtimeInputs;
+            text = ''
+              ${final.babashka}/bin/bb ${script}
+            '';
+            checkPhase = ''
+              ${final.clj-kondo}/bin/clj-kondo ${script}
+            '';
+          };
+      };
+    };
+}
